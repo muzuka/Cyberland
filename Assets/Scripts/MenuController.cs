891e398a4f30824f3549566e049c00e666537b5a
sean brown <seanbrown7935@gmail.com>
2018-12-02 01:22:51 -0700

    Created class for providing functions to the main menu.

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+    public GameObject mainMenu;
+    public GameObject loadoutMenu;
+
+    public void ToLoadout ()
+    {
+        mainMenu.SetActive(false);
+        loadoutMenu.SetActive(true);
+    }
+
+    public void Back ()
+    {
+        mainMenu.SetActive(true);
+        loadoutMenu.SetActive(false);
+    }
+
+    public void Start()
+    {
+        SceneManager.LoadScene("LevelOne");
+    }
+
+    public void Quit ()
+    {
+        Application.Quit();
+    }
+}
