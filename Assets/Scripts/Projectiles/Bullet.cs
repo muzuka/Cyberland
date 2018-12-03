0ccc2e37d023afb89b16218c35c7c12d8ff79ae5
sean brown <seanbrown7935@gmail.com>
2018-12-03 01:08:28 -0700

    Class for a bullet. Gets destroyed on collision.

---
+++ Assets/Scripts/Projectiles/Bullet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : Projectile
+{
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Attackable>())
+        {
+            collision.gameObject.GetComponent<Attackable>().attacked(damage);
+        }
+        Destroy(gameObject);
+    }
+}
