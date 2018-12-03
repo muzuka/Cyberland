a9b3878d042f24c3e7dc9d76547ad904e8fbc991
sean brown <seanbrown7935@gmail.com>
2018-12-03 01:07:59 -0700

    New class for representing a projectile.

---
+++ Assets/Scripts/Projectiles/Projectile.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float damage;
+    public float lifetime;
+    public float speed;
+
+    Timer timer;
+
+    private Vector3 direction;
+    public Vector3 Direction
+    {
+        get
+        {
+            return direction;
+        }
+        set
+        {
+            direction = value.normalized;
+        }
+    }
+
+    public virtual void Start()
+    {
+        timer = new Timer(lifetime);
+    }
+
+    // Use this for initialization
+    public virtual void Update()
+    {
+        GetComponent<Rigidbody2D>().velocity = direction * speed;
+
+        timer.update(DestroyThis);
+    }
+
+    void DestroyThis()
+    {
+        Destroy(gameObject);
+    }
+}
