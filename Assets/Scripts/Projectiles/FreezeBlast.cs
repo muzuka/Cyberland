2f5280b0b11b2a268e8059009a8513060c1c89e4
sean brown <seanbrown7935@gmail.com>
2018-12-03 01:09:43 -0700

    New freeze blast class that damages touching objects over time.

---
+++ Assets/Scripts/Projectiles/FreezeBlast.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreezeBlast : Projectile 
+{
+    public float timePerHit;
+
+    Timer damageTimer;
+
+    List<Attackable> targets;
+
+    public override void Start ()
+    {
+        base.Start();
+        targets = new List<Attackable>();
+        damageTimer = new Timer(timePerHit);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Attackable>())
+        {
+            targets.Add(collision.GetComponent<Attackable>());
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        damageTimer.update(Damage);
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Attackable>())
+        {
+            targets.Remove(collision.GetComponent<Attackable>());
+        }
+    }
+
+    void Damage () 
+    {
+        foreach (Attackable target in targets)
+        {
+            target.attacked(damage);
+        }
+    }
+}
