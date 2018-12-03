f11ebfcfa15a4b6d025f0a3dbdaa6efb85c182ce
sean brown <seanbrown7935@gmail.com>
2018-12-03 01:11:17 -0700

    New class for transferring chosen guns to game.

---
+++ Assets/Scripts/SelectedGuns.cs
@@ -0,0 +1,13 @@
+﻿public class SelectedGuns 
+{
+    public static GunItem gun1;
+    public static GunItem gun2;
+    public static GunItem gun3;
+
+    public static void clearGuns ()
+    {
+        gun1 = null;
+        gun2 = null;
+        gun3 = null;
+    }
+}
