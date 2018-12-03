a1ed50c505485103edc52f96b350f9f0d1e69552
sean brown <seanbrown7935@gmail.com>
2018-12-03 01:13:09 -0700

    New class for an explosion. Applies damage to those in the radius. Disappears after timer.

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour 
+{
+    public float timeToDestroy;
+
+    public float damage { get; set; }
+
+    Timer destroyTimer;
+
+    void Start ()
+    {
+        destroyTimer = new Timer(timeToDestroy);
+    }
+
+    void Update ()
+    {
+        destroyTimer.update(Destroyed);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Attackable>())
+        {
+            collision.GetComponent<Attackable>().attacked(damage);
+        }
+    }
+
+    void Destroyed ()
+    {
+        Destroy(gameObject);
+    }
+}
