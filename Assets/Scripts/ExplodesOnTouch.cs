979bd52fd593d837bb51c4a7274f572124d8d932
sean brown <seanbrown7935@gmail.com>
2018-12-03 01:13:29 -0700

    New class initiates an explosion upon collision.

---
+++ Assets/Scripts/ExplodesOnTouch.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodesOnTouch : MonoBehaviour 
+{
+    public GameObject explosion;
+    public float damage;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Explode();
+    }
+
+    public void Explode()
+    {
+        Instantiate(explosion, transform.position, Quaternion.identity).GetComponent<Explosion>().damage = damage;
+        Destroy(gameObject);
+    }
+}
