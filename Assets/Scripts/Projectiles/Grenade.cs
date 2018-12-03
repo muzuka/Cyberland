5e5dbe88af8ae5df64dfbf05dc606ea53bc9da87
sean brown <seanbrown7935@gmail.com>
2018-12-03 01:10:03 -0700

    New grenade class has a starting speed and explodes over time.

---
+++ Assets/Scripts/Projectiles/Grenade.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grenade : Projectile 
+{
+    public float timeToExplode;
+
+    Timer explosionTimer;
+
+    private Vector3 direction;
+    public Vector3 Direction
+    {
+        get
+        {
+            return direction;
+        }
+        set
+        {
+            direction = value.normalized;
+        }
+    }
+
+    // Use this for initialization
+    void Start () 
+    {
+        GetComponent<Rigidbody2D>().velocity = direction * speed;
+        explosionTimer = new Timer(timeToExplode);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        explosionTimer.update(Explode);
+	}
+
+    void Explode ()
+    {
+        GetComponent<ExplodesOnTouch>().Explode();
+    }
+}
